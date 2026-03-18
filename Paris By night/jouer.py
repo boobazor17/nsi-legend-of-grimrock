baf1ac272e6c1bd800edc6de967c4a51a905fdc4
boobazor17 <beaugrandian@gmail.com>
2026-03-18 22:50:04 +0100

    Update jouer.py

--- Paris By night/jouer.py
+++ Paris By night/jouer.py
@@ -1,6 +1,11 @@
 import pygame
 pygame.init()
 
+x=300 
+y=200
+speed=50
+
+
 #création de la classe contenant les caractéristiques des personnages
 class personnage:
     def __init__(self, nom, pv, pvmax, attaque):
@@ -16,14 +21,28 @@ Rat = personnage("Rat", 50, 50, 10)
 pygame.display.set_caption("Fenêtre d'accueil")
 screen = pygame.display.set_mode((1080,720 ))
 
-running = True
+running = True 
+clock = pygame.time.Clock()
 while running == True:
+    clock.tick(20)
     for event in pygame.event.get():
         screen.fill((201,158,89))
+    
 
 
-pygame.display.update()
+    touches = pygame.key.get_pressed()
+    if touches[pygame.K_z] or touches[pygame.K_UP]:
+        y -= speed
+    if touches[pygame.K_s] or touches[pygame.K_DOWN]:
+        y += speed
+    if touches[pygame.K_q] or touches[pygame.K_LEFT]:
+        x -= speed
+    if touches[pygame.K_d] or touches[pygame.K_RIGHT]:
+        x += speed
+    pygame.display.update()
+    pygame.draw.circle(screen,(255, 0, 0),(x,y),20) 
+    pygame.display.update()
 
-if event.type == pygame.QUIT:
+    if event.type == pygame.QUIT:
             running = False
             pygame.quit()
