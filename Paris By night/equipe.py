4616cb5091ca70b4040b8df82feb634f105c5ee3
boobazor17 <beaugrandian@gmail.com>
2026-04-23 01:42:07 +0200

    création de l'attaque de l'équipe

--- Paris By night/equipe.py
+++ Paris By night/equipe.py
@@ -1,5 +1,7 @@
 import pygame
 import os
+import math
+from Physique import projectile
 pygame.init()
 width = 1080
 height = 720
@@ -12,7 +14,7 @@ class equipe :
                 self.pvmax = pvmax
                 self.degat_attaque = degat_attaque
                 self.distance_attaque = distance_attaque
-                self.attaque = attaque
+                self.attaque = None 
                 if Image: # s'il y a une image
                     chemin = os.path.join(os.path.dirname(__file__), Image) #os.path.dirname(__file__) récupère le dossier où se trouve physique.py, puis os.path.join colle le chemin de l'image dessus
                     self.image = pygame.image.load(chemin).convert_alpha()
@@ -20,36 +22,60 @@ class equipe :
     
     
     def ajouter_attaque(self, attaque):
-        self.attaque.utiliser(attaque)
+        self.attaque = attaque
 
     
 
 class attaque:
-    def __init__(self,nom,degat,portée,rayon,ralentissement,temps_recharge, ): 
+    def __init__(self,nom,degat,portée,rayon,ralentissement,temps_recharge): 
         self.nom = nom
         self.degat = degat
         self.portée= portée
         self.rayon= rayon
         self.ralentissement= ralentissement
         self.temps_recharge= temps_recharge
+        self.attaque_dernier_temps = -1000
+        self.proj = projectile(0, 0, 5, 8, 0, 0, 10) # (x,y) = (0,0)
+        self.monstre = None
     
     
-    def utiliser(self, attaquant):
-        '''ici on vérifierait la distance entre l'attaquant et la cible, et si elle est 
-        inférieure ou égale à la portée de l'attaque, on infligerait les dégâts à la cible 
-        plus les effets eventuels (ralentissement, etc.)'''
-        pass
+    def utiliser(self, attaquant,list_ennemi,player,list_object,liste_equipe):
+        l =[]
+        if len(list_ennemi) != 0:
+            for monstre in list_ennemi:
+                dx = monstre.position.x - player.rect.centerx
+                dy = monstre.position.y - player.rect.centery
+                distance_reelle = math.sqrt(int(dx**2 + dy**2))
+                l.append((distance_reelle, monstre))
+            l.sort()  #trie par distance
+            monstre = l[0][1] # on récupère le monstre
+            self.monstre =  monstre
+            temps =  pygame.time.get_ticks() 
+            if self.nom == "cac":
+                if l[0][0] <= self.portée:
+                    monstre.pv -= self.degat
+            elif self.nom == "distance":
+                if l[0][0] <= self.portée and temps - self.attaque_dernier_temps >= self.temps_recharge :
+                        self.attaque_dernier_temps = temps
+                        if attaquant.pv >= 0: 
+                            self.proj.lancer(pygame.math.Vector2(player.position), monstre)
 
 
-
-
-
-
-
-
-
-
-
+    def update(self, liste_equipe,list_object,temps):
+            if self.nom == "distance" and self.proj.proj_actif:
+                if self.monstre is not None :
+                    if self.proj.proj_actif and temps - self.attaque_dernier_temps <= 1500:
+                        self.proj.position_proj += (self.proj.proj_vitesse_x, self.proj.proj_vitesse_y)
+                        self.proj.collisions(self.monstre,liste_equipe)  
+                    else:
+                        self.proj.proj_actif = False 
+                for object in list_object: # check tous les objets de la liste pour voir s'il y a une collision avec le projectile
+                    if object.rect.collidepoint(self.proj.position_proj):    
+                                self.proj.proj_actif = False          
+    def draw_proj(self,screen,follow):
+        if  self.proj.proj_actif:
+            pygame.draw.circle(screen, (255, 165, 0), follow.appliquer(self.proj.position_proj), self.proj.proj_rayon)
+        
 
     """ def personnage():
      for i in range(4):  # pour des équipes de 4
@@ -69,9 +95,26 @@ class attaque:
     if [choix] not in equipe:  #pour éviter que un personnage soit choisi plusieurs fois par erreur
         equipe.append([choix])
     else:
-        print("Déjà choisi !") """
+        print("Déjà choisi !") """                
+
+def regarde_clique(pos_souris,liste_equipe,list_ennemi,player,list_object):
+        hpb_w = 1/5 * width
+        hpb_h = 1/5* width
+        hpb_x = width - hpb_w -20 
+        hpb_y = height - hpb_h -20
+        for i in range(2):
+                for j in range(2):
+                    index = i*2 + j
+                    case_x = hpb_x + j * 100 + 10  # j pour les colonnes, i pour les lignes
+                    case_y = hpb_y + i * 100 + 10
+                    case_rect = pygame.Rect(case_x-5, case_y-5, 50, 50)
+                    if case_rect.collidepoint(pos_souris):
+                        attaquant = liste_equipe[index]
+                        attaquant.attaque.utiliser(attaquant,list_ennemi,player,list_object,liste_equipe)
+
+
     
-    def changer_equipe(self,taille_equipe,liste_ts,liste_equipe):
+def changer_equipe(self,taille_equipe,liste_ts,liste_equipe):
             pass
 
 def afficher_equipe(liste_equipe,screen):
@@ -107,3 +150,4 @@ def afficher_pv (liste_equipe,screen):
                 pygame.draw.rect(screen, (0, 0, 0), ((case_x-5), case_y, 70, 10)) 
                 if pv > 0:
                     pygame.draw.rect(screen, (200, 0, 0), ((case_x-5), case_y, 70 * (pv/pvmax),10))  # bordure
+
