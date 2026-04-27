6e2f52d549d8062f7c9cd980081e7770a0bf1f1e
boobazor17 <beaugrandian@gmail.com>
2026-04-27 15:35:40 +0200

    Add files via upload

---
+++ Paris By night/assets/icons.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="icons" tilewidth="1024" tileheight="1024" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="vase.png" width="1024" height="1024"/>
+ </tile>
+</tileset>
