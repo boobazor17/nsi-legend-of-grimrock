de8fed36b5b48059f606368e73b2f647b624451d
leoticl <leoti.charleslandier@gmail.com>
2026-04-28 13:18:06 +0000

    Update Physique.py

--- Paris By night/Physique.py
+++ Paris By night/Physique.py
@@ -94,79 +94,7 @@ class item(Object): # tout ce qui est dans l'inventaire
     # potion_de_soin = item("potion de soin", 50)
     
 
-class inventaire:
-    def __init__(self):
-        self.items = []
-        self.item_utilise = None
-        self.perso_choisi = None
-        self.index = None
-        self.x=None
-        self.y = None
 
-    def ajouter(self, nouvel_item):
-        self.items.append(nouvel_item)
-        
-
-    def draw(self, screen,liste_equipe):
-
-        # affiche les 16 cases même vides
-        for i in range(4):
-            for j in range(4):
-                index = i * 4 + j
-                case_x = 590 + j * 45  # j pour les colonnes, i pour les lignes
-                case_y = 280 + i * 45
-                pygame.draw.rect(screen, (100, 80, 60), (case_x, case_y, 42, 42))
-                pygame.draw.rect(screen, (60, 40, 20), (case_x, case_y, 42, 42), 2)  # bordure
-
-            # mettre dans le même bloc à la suite car Quand Python sort du for j, case_x gardait la dernière valeur calculée soit j=3. C'est pour ça que l'image apparaîssait toujours à droite.
-                if index < len(self.items):
-                    image = self.items[index].image
-                    screen.blit(image, (case_x, case_y))
-
-        for i in range(len(liste_equipe)):
-            case_xx = 270 
-            case_jj = 240 + i*55
-            image = liste_equipe[i].image
-            screen.blit(image, (case_xx, case_jj))
-            
-        if self.index is not None:
-            overlay = pygame.Surface((50, 50))
-            overlay.set_alpha(100)  # 0 = invisible, 255 = opaque
-            overlay.fill((50, 50, 50))  
-            screen.blit(overlay, (self.x, self.y+10))
-                    
-    def utiliser(self, pos_souris,liste_equipe):
-        if self.item_utilise is None:
-                    for i in range(4):
-                        for j in range(4):
-                            index = i * 4 + j #  permet de calculer l'index de l'item dans la liste à partir de sa position dans la grille (i, j)
-                            case_x = 590 + j * 45
-                            case_y = 270 + i * 45
-                            case_rect = pygame.Rect(case_x, case_y, 42, 42)
-                            if case_rect.collidepoint(pos_souris): # vérifie si la position de la souris est dans la case cliquée
-                                    if index < len(self.items): #vérifie s'il Quand Python sort du for j, case_x garde la dernière valeur calculée soit j=3. C'est pour ça que l'image apparaît toujours à droite.
-                                        self.item_utilise = self.items[index]
-                                        self.index = index
-                                        self.x= case_x
-                                        self.y = case_y
-
-        else:
-                    for elem in range(len(liste_equipe)):
-                        case_xx = 270 
-                        case_jj = 240 + elem*55
-                        elem_rect= pygame.Rect(case_xx, case_jj, 100, 100)
-                        if elem_rect.collidepoint(pos_souris):
-                            self.perso_choisi = liste_equipe[elem]                 
-        if self.perso_choisi and self.item_utilise:
-            self.perso_choisi.pv += self.item_utilise.effet
-            if self.perso_choisi.pv > self.perso_choisi.pvmax:
-                self.perso_choisi.pv = self.perso_choisi.pvmax
-            self.items.pop(self.index) 
-            self.perso_choisi = None   
-            self.item_utilise = None
-            self.index = None
-            self.c = None
-            self.t = None
                     
 class projectile:                
         def __init__(self,x,y,proj_vitesse,proj_rayon,proj_vitesse_x,proj_vitesse_y,proj_degat,zone,sound_lancer=None,sound_toucher=None):
