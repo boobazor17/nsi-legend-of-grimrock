b9338ad75383eee023d45059911e3daf7dbd4b85
boobazor17 <beaugrandian@gmail.com>
2026-05-03 04:12:26 +0200

    Update monstre.py
    Refactor monster classes to include attack cooldown and speed attributes. Add new monster types and improve movement and attack logic.

--- Paris By night/monstre.py
+++ Paris By night/monstre.py
@@ -2,16 +2,18 @@ import pygame
 import math
 from camera import *
 pygame.init()
-from Physique import Physique  
+from Physique import Physique
 from Physique import projectile
 from Physique import Cac
 import os
+from random import randint
 
                 
 class monstre(Physique):
-    def __init__(self, x, y, nom, pv, pvmax, attaque, distance, distance_attaque):
+    def __init__(self, x, y, nom, pv, pvmax, attaque, distance, distance_attaque, attaque_cooldown, speed):
         super().__init__(x,y,40,40)
         self.position = pygame.math.Vector2(x,y)
+        self.direction = pygame.math.Vector2(0, 0)
         self.nom = nom
         self.pv = pv
         self.pvmax = pvmax
@@ -19,21 +21,14 @@ class monstre(Physique):
         self.distance = distance 
         self.distance_attaque = distance_attaque
         self.attaque_dernier_temps = - 1000
-         
-        
-class monstre_rodeur(monstre):
-    def __init__(self, x, y, nom, pv, pvmax, attaque, distance, distance_attaque,speed):
-        super().__init__(x, y, nom, pv, pvmax, attaque, distance, distance_attaque)
-        self.waypoints = [self.position.copy(), self.position+(200,0), self.position+(200,200), self.position+(0,200)]  # patrouille ici
-        self.waypoint_actuel = 0
-        self.attaque_cooldown = 400
-        self.speed =speed
-        self.direction = pygame.math.Vector2(0, 0)
-
+        self.has_ronde = False
+        self.attaque_cooldown = attaque_cooldown
+        self.speed = speed
 
     def deplacement(self,player,l,list_object):
             self.velocity.x = 0
             self.velocity.y = 0
+            self.direction = self.velocity
             if self.pv > 0:
                 dx = self.position.x - player.rect.centerx
                 dy = self.position.y - player.rect.centery
@@ -47,7 +42,7 @@ class monstre_rodeur(monstre):
                        
                             self.velocity.y -= (dy / distance_reelle) * 2*self.speed/3  # normalisé
 
-                else:  # ronde — on récupère le waypoint actuel
+                if self.has_ronde :  # ronde — on récupère le waypoint actuel
                     cible_x, cible_y = self.waypoints[self.waypoint_actuel]
                     distance_x = cible_x - self.position.x 
                     distance_y = cible_y - self.position.y 
@@ -68,16 +63,98 @@ class monstre_rodeur(monstre):
             if self.velocity.length() > 0:
                 self.direction = self.velocity.normalize()
 
+        
+class monstre_summoner(monstre):
+    def __init__(self, x, y, nom, pv, pvmax, attaque, distance, distance_attaque, attaque_cooldown, speed, summon_max, summon_cooldown,ligne,colonne, classe, Image= None ):
+        super().__init__(x, y, nom, pv, pvmax, attaque, distance, distance_attaque, attaque_cooldown, speed)
+        self.speed =speed
+        self.summon_cooldown = summon_cooldown  # 5 secondes
+        self.summon_dernier_temps = -1000
+        self.summon_number = []
+        self.summon_max = summon_max
+        self.ligne = ligne
+        self.colonne = colonne
+        self.classe = classe
+
+    
+
+    def summon(self, classe, list_ennemi):
+        ennemi = classe(self.position.x + randint(-50,50),self.position.y + randint(-50,50))
+        ennemi.rect.center = ennemi.position
+        self.summon_number.append(ennemi)
+        list_ennemi.append(ennemi)
+
+    def draw(self, screen, follow, player):
+        pass
+
+         
+
+    def attaque_m(self, player, liste_equipe, list_object, list_ennemi, classe):
+        if self.pv > 0:
+            dx = self.position.x - player.rect.centerx
+            dy = self.position.y - player.rect.centery
+            distance_reelle = math.sqrt(int(dx**2 + dy**2))
+            temps =  pygame.time.get_ticks()
+            if  distance_reelle <= self.distance_attaque and player.pv > 0:
+                if len(self.summon_number) < self.summon_max and temps - self.summon_dernier_temps >= self.summon_cooldown:
+                    self.summon_dernier_temps = temps
+                    self.summon(classe, list_ennemi)
+
+class bat_summoner(monstre_summoner):
+    def __init__(self, x, y):
+        super().__init__(x, y, "necromancien", 100, 100, 0, 300, 200, 500, 4, 4, 5000, 4, 4, bat, Image= None )
+
+class bat(monstre):
+    def __init__(self, x, y):
+        super().__init__(x, y, "bat", 30, 30, 10, 100, 70, 500, 8 )
+        self.summon_dernier_temps = -1000
+        self.cac = Cac(0, 0, 20, 20, 10, 50) 
+        self.direction_choisie = 0
+
+    def attaque_m(self, player, liste_equipe, list_object, list_ennemi, classe):
+        if self.pv > 0:
+            dx = self.position.x - player.rect.centerx
+            dy = self.position.y - player.rect.centery
+            distance_reelle = math.sqrt(int(dx**2 + dy**2)) # avec le théoreme de Pythagore on calcule la distance entre le monstre et le joueur
+            temps =  pygame.time.get_ticks()
+            if distance_reelle <self.distance_attaque and temps - self.attaque_dernier_temps >= self.attaque_cooldown :
+                        self.attaque_dernier_temps = temps
+                        if temps - player.invincible_temps >= player.duree_invincibilite and player.pv > 0:  
+                            self.cac.lancer(self.position,player)
+                            self.animation_cac_active = True
+                            self.animation_cac_debut = pygame.time.get_ticks()
+                            self.direction_attaque = self.direction_choisie
+            if self.cac.cac_actif and temps - self.attaque_dernier_temps <= 500:
+                self.cac.collisions(player,list_ennemi,liste_equipe)            
+            else:
+                self.cac.cac_actif = False
+
+    def draw(self, screen, follow, player):
+        if self.pv > 0:
+            pygame.draw.circle(screen, (128, 0, 128), follow.appliquer(self.position), 15)
+     
+
+class monstre_rodeur(monstre):
+    def __init__(self, x, y, nom, pv, pvmax, attaque, distance, distance_attaque, attaque_cooldown, speed):
+        super().__init__(x, y, nom, pv, pvmax, attaque, distance, distance_attaque, attaque_cooldown, speed)
+        self.waypoints = [self.position.copy(), self.position+(200,0), self.position+(200,200), self.position+(0,200)]  # patrouille ici
+        self.waypoint_actuel = 0
+        self.speed =speed
+        self.has_ronde = True
+        self.attaque_cooldown = attaque_cooldown
+
+
 
 class araignee(monstre_rodeur):
     def __init__(self, x, y):
-        super().__init__(x, y, "araignee", 80, 80, 15, 200, 100, 8)
+        super().__init__(x, y, "araignee", 80, 80, 15, 200, 100, 700, 6)
         self.cac = Cac(0, 0, 35, 35, 10, 100)
         self.rect = pygame.Rect(x, y, 40, 40)
-        self.speed = 10
         self.a = None # variable
         chemin = os.path.join(os.path.dirname(__file__), "assets/araignee_spritesheet.png")
         spritesheet = pygame.image.load(chemin).convert_alpha()
+        chemin = os.path.join(os.path.dirname(__file__), "assets/aa.png")
+        attaque_sheet = pygame.image.load(chemin).convert_alpha()
 
         self.frames = []
         frame_width = 110
@@ -101,17 +178,48 @@ class araignee(monstre_rodeur):
         self.derniere_frame = 0
         self.direction_choisie = 0
 
+        # Si le fond noir n'est pas transparent :
+        attaque_sheet.set_colorkey((0, 0, 0))
 
-    def attaque_m(self,player,liste_equipe,list_object,list_ennemi):
+        self.frames_attaque = []
+
+        colonnes_attaque = 5
+        lignes_attaque = 8
+        frame_w = attaque_sheet.get_width() // colonnes_attaque
+        frame_h = attaque_sheet.get_height() // lignes_attaque
+
+        for ligne in range(lignes_attaque):
+            ligne_frames = []
+            for colonne in range(colonnes_attaque):
+                frame = attaque_sheet.subsurface(
+                    pygame.Rect(
+                        colonne * frame_w,
+                        ligne * frame_h,
+                        frame_w,
+                        frame_h
+                    )
+                )
+                ligne_frames.append(frame)
+            self.frames_attaque.append(ligne_frames)
+        self.animation_cac_active = False
+        self.animation_cac_debut = 0
+        self.direction_attaque = 0
+
+
+
+    def attaque_m(self,player,liste_equipe,list_object,list_ennemi, classe):
         if self.pv > 0:
             dx = self.position.x - player.rect.centerx
             dy = self.position.y - player.rect.centery
             distance_reelle = math.sqrt(int(dx**2 + dy**2)) # avec le théoreme de Pythagore on calcule la distance entre le monstre et le joueur
             temps =  pygame.time.get_ticks()
-            if distance_reelle <self.distance and temps - self.attaque_dernier_temps >= self.attaque_cooldown :
+            if distance_reelle <self.distance_attaque and temps - self.attaque_dernier_temps >= self.attaque_cooldown :
                         self.attaque_dernier_temps = temps
                         if temps - player.invincible_temps >= player.duree_invincibilite and player.pv > 0:  
                             self.cac.lancer(self.position,player)
+                            self.animation_cac_active = True
+                            self.animation_cac_debut = pygame.time.get_ticks()
+                            self.direction_attaque = self.direction_choisie
             if self.cac.cac_actif and temps - self.attaque_dernier_temps <= 500:
                 self.cac.collisions(player,list_ennemi,liste_equipe)            
             else:
@@ -148,9 +256,10 @@ class araignee(monstre_rodeur):
 
                     # 2. avancer l’animation
                     temps = pygame.time.get_ticks()
-                    if temps - self.derniere_frame > 120:
-                        self.frame_index = (self.frame_index + 1) % 4
-                        self.derniere_frame = temps
+                    if self.velocity.length() > 0:
+                        if temps - self.derniere_frame > 120:
+                            self.frame_index = (self.frame_index + 1) % 4
+                            self.derniere_frame = temps
 
                     # 3. choisir une seule image
                     image = self.frames[self.frame_index][self.direction_choisie]
@@ -161,17 +270,37 @@ class araignee(monstre_rodeur):
                     y = pos[1] - image.get_height() // 2
                     screen.blit(image, (x, y))
 
+                    if self.animation_cac_active and self.cac.cac_rect is not None and player.pv > 0:
+                        temps_attaque = pygame.time.get_ticks() - self.animation_cac_debut
+                        frame_attaque = temps_attaque // 60
+
+                        if frame_attaque < 5:
+                            correspondance_attaque = {
+                                0: 2,  # bas
+                                1: 6,  # haut
+                                2: 0,
+                                3: 4,
+                                4: 7,
+                                5: 5,
+                                6: 3,
+                                7: 1,
+                            }
 
 
-                    if self.cac.cac_actif :
-                        self.a = pygame.time.get_ticks()
-                    if self.a is not None and player.pv > 0:
-                        if self.a - self.attaque_dernier_temps< 150: 
-                            pygame.draw.rect(screen, (255, 0, 0), (self.cac.cac_rect.x - follow.camera.offset.x, self.cac.cac_rect.y - follow.camera.offset.y, self.cac.cac_largeur,self.cac.cac_hauteur))
+                            ligne_attaque = correspondance_attaque[self.direction_attaque]
+                            image_attaque = self.frames_attaque[ligne_attaque][frame_attaque]
+
+                            x = self.cac.cac_rect.centerx - follow.camera.offset.x - image_attaque.get_width() // 2
+                            y = self.cac.cac_rect.centery - follow.camera.offset.y - image_attaque.get_height() // 2
+
+                            screen.blit(image_attaque, (x, y))
+                        else:
+                            self.animation_cac_active = False
+
 
 class ennemi1(monstre_rodeur):
     def __init__(self, x, y):
-        super().__init__(x, y, "ennemi1", 100, 100, 10, 250, 130, 10)
+        super().__init__(x, y, "ennemi1", 100, 100, 10, 250, 130, 600, 10)
         self.proj = projectile(x, y, 5, 8, 0, 0, 10,0) # initialisation du projectile (position(x,y)  vitesse , rayon, vitesse_x, vitesse_y, degat)
         self.rect = pygame.Rect(x, y, 40, 40)
         self.speed = 10
@@ -188,7 +317,7 @@ class ennemi1(monstre_rodeur):
                     pygame.draw.circle(screen, (255, 165, 0), follow.appliquer(self.proj.position_proj), self.proj.proj_rayon)
 
     
-    def attaque_m(self,player,liste_equipe,list_object,list_ennemi):
+    def attaque_m(self, player, liste_equipe, list_object, list_ennemi, classe):
             if self.pv > 0:
                 dx = self.position.x - player.rect.centerx
                 dy = self.position.y - player.rect.centery
@@ -214,7 +343,7 @@ class ennemi1(monstre_rodeur):
     
         
 
-    def dash(self,player,liste_equipe,degat):
+    def dash(self, player, liste_equipe, degat):
                 if self.pv <= 0 :   #plus optimal que de faire un if à chaque fois dans les fonctions de déplacement et d'attaque, on sort directement de la fonction si le monstre est mort
                     return
                 temps =  pygame.time.get_ticks() 
