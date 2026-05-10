53a909b44fc3211c6ce82ce72cf29bdb39ff44cf
leoticl <leoti.charleslandier@gmail.com>
2026-05-10 23:42:00 +0200

    Enhance credits display with developer information
    Updated the credits display to include detailed information about the developers and sponsors, along with proper formatting and color coding.

--- Paris By night/credits.py
+++ Paris By night/credits.py
@@ -1,9 +1,38 @@
 import pygame
 pygame.init()
 
-
 def afficher_credits(screen, font):
+
     screen.fill((0, 0, 0))
-    
-    texte = font.render("ici ce sera les credits dcp", True, (16711935))
-    screen.blit(texte, (450, 300))
+
+    credits = [ "LEGEND OF GRIMROCK",
+                "", 
+                "Par les développeurs :", 
+                "leoti, Ian, Roxane er Lola", 
+                "", 
+                "Avec le parainage de :",
+                "Guillaume et lounis",
+                "", 
+                "Afin de satisfaire les exigences démoniaques de :",
+                "Monsieur Rossier", 
+                "", 
+                "Merci d'avoir joué !" ]
+
+    y = 120
+
+    for i, ligne in enumerate(credits):
+
+        # couleur du titre
+        if i == 0:
+            couleur = (167, 119, 255)
+        else:
+            couleur = (255, 255, 255)
+
+        texte = font.render(ligne, True, couleur)
+
+        # centrage horizontal
+        x = screen.get_width() // 2 - texte.get_width() // 2
+
+        screen.blit(texte, (x, y))
+
+        y += 50
